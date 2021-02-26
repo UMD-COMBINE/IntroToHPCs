5a460a2b342c7eb296edadf42372e704acd5c2f9
JunaidMerchant <merchantjs@gmail.com>
2021-02-26 03:23:11 -0500

    matlab example 1 code

---
+++ MatlabExample1_wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --time=168:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=24000
+#SBATCH --output=MatlabExample1.log
+#SBATCH --mail-user=jmerch@terpmail.umd.edu
+#SBATCH --mail-type=ALL
+#
+# This is the simplest way of running a single piece of matlab code. You would submit this script, 
+# which will launch matlab and run the accompanying matlab code for an Ising model.
+# This approach is not great for running things in parallel, but gives you an idea of how job submission
+# works. 
+#
+# to run this on a compute node:
+# > sbatch /path/to/MatlabExample1_Ising_wrapper.sh
+#
+# load the matlab module
+module load matlab
+matlab -nodesktop -nodisplay -nosplash -r "clear; run /data/bswift-1/jmerch/P2P/MatlabExample1_MonteCarloSimulation.m ; exit"
